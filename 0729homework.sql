d298077831c854bcb0fdac087a2f23a6dfd506b7
yiwen902 <yiwen902@gmail.com>
2019-07-30 11:16:30 +0800

    HOMEWORK

--- 0729homework.sql
+++ 0729homework.sql
@@ -1,5 +1,8 @@
+--所有屋子裡的住戶清單
 SELECT house.hid,house.address,userinfo.uid,userinfo.cname
 FROM house RIGHT JOIN live ON
      house.hid = live.hid  
      RIGHT JOIN userinfo ON
      live.uid = userinfo.uid
+
+    
