fe96addbd54b337582eb2b1a260448efad68494c
yiwen902 <yiwen902@gmail.com>
2019-07-30 14:20:21 +0800

    excise

--- 0730練習.sql
+++ 0730練習.sql
@@ -1,3 +1,21 @@
+--極端值
+SELECT tel,max_sum_fee
+FROM
+(
+    SELECT max(sum_fee) as max_sum_fee
+    FROM (
+         select tel,sum(fee) as sum_fee
+         from bill
+         GROUP BY tel
+         ) as a 
+)as aa,(
+    select tel,sum(fee)as sum_fee
+    from bill
+    group by tel
+      ) as bb
+WHERE aa.max_sum_fee = bb.sum_fee
+
+
 --哪個人擁有最多電話
 select uid,max_n
 FROM(
@@ -17,8 +35,40 @@ FROM(
 WHERE aa.max_n = bb.n
 
 
+--使用limit
+SELECT tel,aa.sum_fee
+FROM(
+     select sum(fee) as sum_fee
+     from bill
+     GROUP BY tel
+     ORDER BY sum_fee DESC
+     limit 1
+    )as aa,(
+    select tel,sum(fee) as sum_fee
+    from bill
+    group by tel
+    ) as bb
+WHERE aa.sum_fee = bb.sum_fee
 
 
+--視觀表 VIEW
 
+create view vw_檢視表 as 
+SELECT tel,aa.sum_fee
+FROM(
+     select sum(fee) as sum_fee
+     from bill
+     GROUP BY tel
+     ORDER BY sum_fee DESC
+     limit 1
+    )as aa,(
+    select tel,sum(fee) as sum_fee
+    from bill
+    group by tel
+    ) as bb
+WHERE aa.sum_fee = bb.sum_fee
+
+--以上建立之後，可以用以下語法查看到表格內容
+select * from vw_視觀表
 
 
