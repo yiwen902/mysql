a6a4181818985eba4c645ecaef92785f5b6520e4
yiwen902 <yiwen902@gmail.com>
2019-07-31 10:17:36 +0800

    LAB

--- 0731作業.sql
+++ 0731作業.sql
@@ -1,6 +1,30 @@
-CREATE TABLE `homework`.`userinfo`
+ CREATE TABLE `homework`.`userinfo`
  ( 
    `UID` VARCHAR(10) NOT NULL , 
-   `cname` VARCHAR(40) NULL ,
-   `u_pic` BLOB NOT NULL , PRIMARY KEY (`UID`)
-) ENGINE = InnoDB;
+   `cname` VARCHAR(40) NOT NULL ,
+   `u_pic` BLOB NOT NULL ,PRIMARY KEY (`UID`)
+ )
+ ENGINE = InnoDB;
+
+ CREATE TABLE `homework`.`orderinfo` 
+ ( 
+   `uid` VARCHAR(10) NOT NULL ,
+   `order_no` int(6) NOT NULL AUTO_INCREMENT  ,
+   `time` DATETIME NOT NULL,PRIMARY KEY (`order_no`)
+ ) 
+ ENGINE = InnoDB;
+
+ CREATE TABLE `homework`.`buy`
+  ( 
+    `order_no` int(6) NOT NULL AUTO_INCREMENT ,
+    `pid` VARCHAR(10) NOT NULL  , 
+    `amout` INT(10) NOT NULL , PRIMARY KEY (`order_no`, `pid`)
+  )
+  ENGINE = InnoDB;
+
+  CREATE TABLE `homework`.`product` 
+  ( 
+    `pid` VARCHAR(10) NOT NULL , 
+    `img` BLOB NOT NULL , PRIMARY KEY (`pid`)
+  ) 
+  ENGINE = InnoDB;
